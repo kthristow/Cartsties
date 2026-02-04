6934596d131e29cfba022fd036be1d63a825f08d
Kaloyan <79564253+kthristow@users.noreply.github.com>
2026-02-04 19:35:58 +0200

    feat: initialize frontend web app with Next.js and TypeScript
    - Added package.json for managing dependencies and scripts.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for UI components.
    - Configured TypeScript with tsconfig.json for type safety.
    - Defined types for Auction and PagedResult in index.ts.
    - Updated Docker configuration for GatewayService to simplify service addresses.

---
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useParamsStore } from "@/hooks/useParamsStore";
+import Heading from "./Heading";
+import { Button } from "flowbite-react";
+
+type Props = {
+  title?: string;
+  subtile?: string;
+  showReset?: boolean;
+};
+
+export default function EmptyFilter({
+  title = "No matches for this filter",
+  subtile = "Try changing your filter criteria",
+  showReset,
+}: Props) {
+  const reset = useParamsStore((state) => state.reset);
+
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center h-[40vh] shadow-lg">
+      <Heading title={title} subtitle={subtile} center />
+      <div className="mt-4">
+        {showReset && (
+          <Button outline onClick={reset}>
+            Remove filters
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+}
